0a5403a5861b054e2aab81b08833a2fbb3e6e81a
jeft1788 <jeft1788@gmail.com>
2023-08-27 15:51:56 -0500

    Mi equipo

---
+++ src/App.java
@@ -0,0 +1,23 @@
+import factorias.EquipoFactory;
+import factorias.EquipoFemeninoFactory;
+import factorias.EquipoMasculinoFactory;
+import partes.Arquero;
+import partes.Atacante;
+import partes.Defensor;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        crearEquipo(new EquipoMasculinoFactory());
+        crearEquipo(new EquipoFemeninoFactory());
+    }
+    public static void crearEquipo(EquipoFactory factory) {
+        Arquero arquero = factory.crearArquero();
+        Defensor defensor = factory.crearDefensor();
+        Atacante atacante = factory.crearAtacante();
+      
+        System.out.println("_______Un equipo fue creado:_____ ");	   	
+        System.out.println(arquero.saqueArco());
+        System.out.println(defensor.barrida());
+        System.out.println(atacante.cabezazoOfensivo());	        
+    }
+}
---
+++ src/partes/ArqueroHombre.java
@@ -0,0 +1,23 @@
+package partes;
+
+public class ArqueroHombre 
+	implements Arquero {
+
+	@Override
+	public String tirarseIzquierda() {
+		return 
+			"tirarse izquierda arquero hombre";
+	}
+
+	@Override
+	public String saqueArco() {
+		return  
+			"saque de arco de arquero hombre";
+	}
+
+	@Override
+	public String tirarseDerecha() {
+		return 
+			"tirarse a derecha arquero hombre";
+	}
+}
